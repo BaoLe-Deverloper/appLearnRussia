6472f180216fb9cf78a295ac1f0dd4130e055b18
Baole <=>
2019-10-15 23:12:32 +0300

    commit 7

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    UrlServerBase: "http://localhost:8000/api/v1/",
+    UrlServerBase: "https://apiapplearnrussian.herokuapp.com/api/v1/",
     newsApi_key:"0ec9bb5b6f5e4ce581155c112e35ca58",
     newsApi_fbclid :"IwAR1JJsvL1cR1QtDQxWiEle8wWRZw-YOgL_ez2bF_oDcFdQzmb0_vWZtcTx8",
     urlApi_Youtube:"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLLHjKKyQ4OaSLC6tLdplgGguAyTHEjUs0&key=AIzaSyD0-0rHKHyZrg-vy0mOwbaIOXt7plwKiHg"
