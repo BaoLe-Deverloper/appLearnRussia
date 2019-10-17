c2b126c52d63da48362619e993a9668939a8e8b7
Baole <=>
2019-10-17 19:43:57 +0300

    Updates

---
+++ precache-manifest.ac198058ae97a8553c4ce49bb5460659.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7813519839b7c639e72cec46b454a4a",
+    "url": "/appLearnRussia/static/media/tick.e7813519.jpg"
+  },
+  {
+    "revision": "6905e3f3c9e9a280274e",
+    "url": "/appLearnRussia/static/css/main.c4466e1f.chunk.css"
+  },
+  {
+    "revision": "d7f9728824c65e3aeb55b3c576872773",
+    "url": "/appLearnRussia/static/media/bg.d7f97288.jpg"
+  },
+  {
+    "revision": "ae6c1c1eb79ab756d400",
+    "url": "/appLearnRussia/static/js/1.ae6c1c1e.chunk.js"
+  },
+  {
+    "revision": "4b2887b3fc41f081fe5a",
+    "url": "/appLearnRussia/static/js/runtime~main.4b2887b3.js"
+  },
+  {
+    "revision": "3c1d695ce578b9721327554026a42779",
+    "url": "/appLearnRussia/static/media/userAvatar.3c1d695c.png"
+  },
+  {
+    "revision": "6905e3f3c9e9a280274e",
+    "url": "/appLearnRussia/static/js/main.6905e3f3.chunk.js"
+  },
+  {
+    "revision": "1e01570b65e9fa1de4b10c49e19b15b8",
+    "url": "/appLearnRussia/static/media/Fail.1e01570b.png"
+  },
+  {
+    "revision": "27e2db495c5b08e73667739670130b46",
+    "url": "/appLearnRussia/static/media/test.27e2db49.jpg"
+  },
+  {
+    "revision": "8916ed982a99df167410545582dd818f",
+    "url": "/appLearnRussia/static/media/media.8916ed98.jpg"
+  },
+  {
+    "revision": "153c202b37b31bc42ddf3d7815c66661",
+    "url": "/appLearnRussia/static/media/11n.153c202b.ico"
+  },
+  {
+    "revision": "ae6c1c1eb79ab756d400",
+    "url": "/appLearnRussia/static/css/1.3daacc57.chunk.css"
+  },
+  {
+    "revision": "eb3bf712503d261640315e9a0e38a451",
+    "url": "/appLearnRussia/index.html"
+  }
+];
